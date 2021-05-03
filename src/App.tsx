5e7c457f37a7f741d0f94dec2994778332e2d3a3
t-keshi <quick.resp.biz094@gmail.com>
2021-05-03 14:36:42 +0900

    feature: firebase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
 import { Providers } from './containers/Providers';
 import { Routes } from './routes/Routes';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2HuHqiV_v8pyFVILMVM3Tvzqg5cLJsgI',
+  authDomain: 'symphony-forum.firebaseapp.com',
+  projectId: 'symphony-forum',
+  storageBucket: 'symphony-forum.appspot.com',
+  messagingSenderId: '790819168126',
+  appId: '1:790819168126:web:9b06137671a85be7181d2f',
+  measurementId: 'G-BZN0NKFS63',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export const App: React.VFC = () => (
   <Router>
