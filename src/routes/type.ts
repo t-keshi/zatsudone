e14de5a8838871efd0e9444bbefd0694902fbfac
t-keshi <quick.resp.biz094@gmail.com>
2021-07-20 15:50:33 +0900

    feature: about

--- src/routes/type.ts
+++ src/routes/type.ts
@@ -13,6 +13,7 @@ export const ROUTE_PATHS = {
   プロフィール設定: '/profile-setting',
   プロフィール: '/profile/:uid',
   マイページ: '/my-page',
+  アバウト: '/about',
 } as const;
 
 export type ROUTE_PATH = typeof ROUTE_PATHS[keyof typeof ROUTE_PATHS];
