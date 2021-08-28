a48ef4a5d82dbdccfdb233b8265f0b751557da24
t-keshi <quick.resp.biz094@gmail.com>
2021-08-28 16:50:19 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import 'firebase/analytics';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppUtility } from './AppUtility';
 import { Providers } from './containers/Providers';
-import { firebaseConfig } from './firebaseConfig';
 import { Routes } from './routes/Routes';
-
-// ---------- firebase ----------
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 export const App: React.VFC = () => (
   <Router>
